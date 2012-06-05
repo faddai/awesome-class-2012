84f935ae04fbc60463143b283f1a79357d0c80f4
Francis Addai <4addai@gmail.com>
2012-06-05 10:15:46 +0000

    Added theme to nav items

--- includes/nav.php
+++ includes/nav.php
@@ -1,17 +1,17 @@
 <div data-role="navbar" data-iconpos="top">
                     <ul>
                         <li>
-                            <a href="#welcome" data-theme="" data-icon="home">
+                            <a href="#welcome" data-theme="b" data-icon="home">
                                 Welcome
                             </a>
                         </li>
                         <li>
-                            <a href="#trainees" data-theme="" data-icon="star"> <!-- class="ui-btn-active"> -->
+                            <a href="#trainees" data-theme="b" data-icon="star"> <!-- class="ui-btn-active"> -->
                                 Trainees
                             </a>
                         </li>
                         <li>
-                            <a href="#instructors" data-theme="" data-icon="check">
+                            <a href="#instructors" data-theme="b" data-icon="check">
                                 Instructors
                             </a>
                         </li>
