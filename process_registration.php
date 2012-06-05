c812cf603c0fa8504cd671fe729bd660af5ae251
Francis Addai <4addai@gmail.com>
2012-06-05 10:17:32 +0000

    fixed error during INSERT, escaped strings to make 'em safe

--- process_registration.php
+++ process_registration.php
@@ -1,26 +1,34 @@
 <?php
 include( "includes/functions.php" );
 
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$gender = $_POST['gender'];
-$dob_day = $_POST['dob_day'];
-$dob_month = $_POST['dob_month'];
-$dob_year = $_POST['dob_year'];
-$interests = $_POST['interests'];
-$bio = $_POST['bio'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
+// Clean up user input and prep it to be safe for the database
+// escape all inputs with mysql_real_escape_string to make the data safe and avoid SQL injection
+
+$fname = mysql_real_escape_string(trim($_POST['fname']));
+$lname = mysql_real_escape_string(trim($_POST['lname']));
+$gender = mysql_real_escape_string(trim($_POST['gender']));
+$dob_day = mysql_real_escape_string(trim($_POST['dob_day']));
+$dob_month = mysql_real_escape_string(trim($_POST['dob_month']));
+$dob_year = mysql_real_escape_string(trim($_POST['dob_year']));
+$interests = mysql_real_escape_string(trim($_POST['interests']));
+$bio = mysql_real_escape_string(trim($_POST['bio']));
+$email = mysql_real_escape_string(trim($_POST['email']));
+$phone = mysql_real_escape_string(trim($_POST['phone']));
 $password = encrypt_password( $_POST['password'] );
-$pix = "trainees/$fname"; # use first name to grab photo from the trainees folder
+$pix = mysql_real_escape_string("trainees/". strtolower($fname). ".jpg"); # use first name to grab photo from the trainees folder. 
 
+// concantenate year, month and day into MySQL accepted format i.e. YYYY-MM-DD
 $dob = "$dob_year-$dob_month-$dob_day";
-
-$res = query("INSERT into members(fname,lname,gender,dob,interests,bio,email,phone,password,pix)
+$sql = "INSERT into members(fname,lname,gender,dob,interests,bio,email,phone,password,pix)
              values('$fname','$lname','$gender','$dob','$interests',
-             '$bio', '$email', '$phone','$password','$pix')"
-             );
+             '$bio', '$email', '$phone','$password','$pix')";
+             
+echo $sql; 
+             
+$res = query($sql);
 
+var_dump($res);
+die();
 if( $res ){
     echo "Thank you for you registering.";
 } else{
