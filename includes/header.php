5a76cf2c20722743afc53cf0f26fafe8383f3904
Francis Addai <4addai@gmail.com>
2012-06-07 16:20:24 +0000

    Changed page title of the app

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8" />
-    <title>Having fun with jQuery Mobile</title>
+    <title>Awesome Class 2012</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="css/jquery.mobile-1.1.0.min.css" />
     
